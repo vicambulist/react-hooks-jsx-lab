5581032bdc0de482913088111121e57faf09ac9d
Bret Barden <bret.barden@gmail.com>
2023-08-08 20:54:04 -0400

    Lab complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
+const h1 = `${name} is a Web Developer from ${city}`;
+
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id ="home">
+      <h1 style = {{ color: "firebrick" }}> {h1} </h1>
+    </div>)
 }
 
 export default Home;
